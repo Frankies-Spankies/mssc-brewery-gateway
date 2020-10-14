5c7e2daf595d5f1faf06bae1b33c3ae25081fdf7
Francisco Gamez <franciscogamezdg@gmail.com>
2020-10-14 09:40:04 -0500

    Hystrix con open feing client

--- src/main/java/com/sfg/gateway/config/LoadBalancedRoutesConfig.java
+++ src/main/java/com/sfg/gateway/config/LoadBalancedRoutesConfig.java
@@ -12,7 +12,7 @@ public class LoadBalancedRoutesConfig {
 
     /**
      * Enruta el trafico de las rutas que se le indican, al servicio gateway
-    * */
+     */
     @Bean
     public RouteLocator loadBalancedRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
@@ -23,8 +23,15 @@ public class LoadBalancedRoutesConfig {
                         .uri("lb://order-service")
                         .id("order-service"))
                 .route(r -> r.path("/api/v1/beer/*/inventory")
+                        //Configura uri en caso de que haya algun problema con el servicio inventory-service
+                        .filters(f -> f.circuitBreaker(c -> c.setName("inventoryCB")
+                                .setFallbackUri("forward:/inventory-failover")
+                                .setRouteId("inv-failover")))
                         .uri("lb://inventory-service")
                         .id("inventory-service"))
+                .route(r -> r.path("/inventory-failover/**")
+                        .uri("lb://inventory-failover")
+                        .id("inventory-failover"))
                 .build();
     }
 }
